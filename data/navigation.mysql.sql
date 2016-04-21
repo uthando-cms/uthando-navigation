8c62ea75e2519cdda95dd7db539201bb066cc533
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-04-21 08:21:24 +0100

    added mysql table data

--- data/navigation.mysql.sql
+++ data/navigation.mysql.sql
@@ -19,36 +19,48 @@ CREATE TABLE IF NOT EXISTS `menu` (
   UNIQUE KEY `menu` (`menu`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `page`
+-- Table structure for table `menuItem`
 --
 
-DROP TABLE IF EXISTS `page`;
-CREATE TABLE IF NOT EXISTS `page` (
-  `pageId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `menuId` int(10) unsigned NOT NULL,
+DROP TABLE IF EXISTS `menuItem`;
+CREATE TABLE `menuItem` (
+  `menuItemId` int(10) UNSIGNED NOT NULL,
+  `menuId` int(10) UNSIGNED NOT NULL,
   `label` varchar(255) NOT NULL,
-  `params` text NOT NULL,
+  `params` text,
   `route` varchar(255) DEFAULT NULL,
   `uri` varchar(255) DEFAULT NULL,
   `resource` varchar(255) NOT NULL,
-  `visible` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `lft` int(10) unsigned NOT NULL,
-  `rgt` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`pageId`),
-  KEY `menuId` (`menuId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `visible` tinyint(1) UNSIGNED NOT NULL DEFAULT '1',
+  `lft` int(10) UNSIGNED NOT NULL,
+  `rgt` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Indexes for table `menuItem`
+--
+ALTER TABLE `menuItem`
+ADD PRIMARY KEY (`menuItemId`),
+ADD KEY `menuId` (`menuId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menuItem`
+--
+ALTER TABLE `menuItem`
+MODIFY `menuItemId` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `page`
+-- Constraints for table `menuItem`
 --
-ALTER TABLE `page`
-  ADD CONSTRAINT `page_ibfk_3` FOREIGN KEY (`menuId`) REFERENCES `menu` (`menuId`);
-  
+ALTER TABLE `menuItem`
+ADD CONSTRAINT `menuItem_ibfk_1` FOREIGN KEY (`menuId`) REFERENCES `menu` (`menuId`) ON DELETE CASCADE;
+
 SET FOREIGN_KEY_CHECKS=1;
